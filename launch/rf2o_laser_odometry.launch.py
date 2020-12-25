702768f596d71c7be364ca6006a73f6dc67e35e9
H-HChen <72009899+H-HChen@users.noreply.github.com>
2020-12-25 17:31:27 +0800

    Update rf2o_laser_odometry.launch.py

--- launch/rf2o_laser_odometry.launch.py
+++ launch/rf2o_laser_odometry.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
                 parameters=[{
                     'laser_scan_topic' : '/scan',
                     'odom_topic' : '/odom_rf2o',
-                    'publish_tf' : False,
+                    'publish_tf' : True,
                     'base_frame_id' : 'base_footprint',
                     'odom_frame_id' : 'odom',
                     'init_pose_from_topic' : '',
